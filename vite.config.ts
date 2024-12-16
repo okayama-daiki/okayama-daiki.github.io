4f6130066d5f45f15bf64173e526ecb078928d81
Daiki Okayama <lawliet.d.k.m@gmail.com>
2024-12-16 15:30:06 +0900

    Enhance Vite configuration for manual chunking

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+          ui: ["@nextui-org/react", "@geist-ui/icons", "framer-motion"],
+          i18n: [
+            "i18next",
+            "i18next-browser-languagedetector",
+            "i18next-http-backend",
+            "react-i18next",
+          ],
+        },
+      },
+    },
+  },
+});
