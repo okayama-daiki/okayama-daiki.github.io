2646993db943c3b20c781295c63c2a5e83ab4e11
Daiki Okayama <lawliet.d.k.m@gmail.com>
2025-05-23 20:08:01 +0900

    Set up Chakura UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+
+import App from "@/App.tsx";
+import { Provider } from "@/components/ui/provider.tsx";
 
 // biome-ignore lint/style/noNonNullAssertion: index.html is guaranteed to have an element with id "root"
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </StrictMode>
 );
