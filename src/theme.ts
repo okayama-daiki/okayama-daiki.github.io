3e5cd2f551a3d2b6f9bce731ba3c31c2a0462bc7
Daiki Okayama <lawliet.d.k.m@gmail.com>
2025-06-19 15:49:39 +0900

    Format source code by biome

--- src/theme.ts
+++ src/theme.ts
@@ -2,21 +2,21 @@ import { createSystem, defaultConfig, defineConfig } from "@chakra-ui/react";
 import "@fontsource/sacramento/index.css";
 
 const customConfig = defineConfig({
-	globalCss: {
-		ul: {
-			paddingLeft: "1.5rem", // In default setting, ul must not have padding
-		},
-	},
-	theme: {
-		tokens: {
-			fonts: {
-				sacramento: {
-					value: "Sacramento, cursive",
-					description: "Sacramento font from @fontsource/sacramento",
-				},
-			},
-		},
-	},
+  globalCss: {
+    ul: {
+      paddingLeft: "1.5rem", // In default setting, ul must not have padding
+    },
+  },
+  theme: {
+    tokens: {
+      fonts: {
+        sacramento: {
+          value: "Sacramento, cursive",
+          description: "Sacramento font from @fontsource/sacramento",
+        },
+      },
+    },
+  },
 });
 
 export const customSystem = createSystem(defaultConfig, customConfig);
